2737c2d4621bdf325e1bc0c1337d429a972d220e
Marc Portier <marc.portier@gmail.com>
2026-02-23 10:20:37 +0100

    adding ip resolving to script - hoping to trace cdn location

--- getimgs.sh
+++ getimgs.sh
@@ -1,25 +1,42 @@
 #! /usr/bin/env bash
 
-base_img_url="https://files.recranet.app/organizations/1631/2025-10-29-13-50-55/p3300073.jpg"
+# base images are served here
+base_host="files.recranet.app" 
+base_img_url="https://${base_host}/organizations/1631/2025-10-29-13-50-55/p3300073.jpg"
 
-variant_img_url="https://recranet.app/cdn-cgi/image/format=auto,width=600,height=280,dpr=1,q=85,fit=crop/${base_img_url}"
+# processed images are served here
+proc_host="recranet.app"
+proc_img_url="https://${proc_host}/cdn-cgi/image/format=auto,width=600,height=280,dpr=1,q=85,fit=crop/${base_img_url}"
 
+# output is storede 
 rpt_file="rpt-$(date --iso).txt" 
 rm -rf ${rpt_file} 2>/dev/null
 
-echo "----"                         >> ${rpt_file}
+echo "===="                         >> ${rpt_file}
+
 echo "my location..."               >> ${rpt_file}
 echo "----"                         >> ${rpt_file}
 curl "http://ip-api.com/json/"      >> ${rpt_file}
-echo  -e "\n----"                   >> ${rpt_file}
+echo  -e "\n===="                   >> ${rpt_file}
+
+echo "IP of servers (cdn location)" >> ${rpt_file}
+echo "----"                         >> ${rpt_file}
+echo "resolved IP for ${base_host}" >> ${rpt_file}
+host "${base_host}"                 >> ${rpt_file}
+echo "resolved IP for ${proc_host}" >> ${rpt_file}
+host "${proc_host}"                 >> ${rpt_file}
+echo "===="                         >> ${rpt_file}
+
 echo "get ${base_img_url}"          >> ${rpt_file}
 echo "----"                         >> ${rpt_file}
 curl -LI --url "${base_img_url}"    >> ${rpt_file}
+echo "===="                         >> ${rpt_file}
+
+echo "get ${proc_img_url}"          >> ${rpt_file}
 echo "----"                         >> ${rpt_file}
-echo "get ${variant_img_url}"       >> ${rpt_file}
-echo "----"                         >> ${rpt_file}
-curl -LI --url "${variant_img_url}" >> ${rpt_file}
-echo "----"                         >> ${rpt_file}
+curl -LI --url "${proc_img_url}"    >> ${rpt_file}
+echo "===="                         >> ${rpt_file}
+
 cat ${rpt_file}
 echo "done"
 exit 0
