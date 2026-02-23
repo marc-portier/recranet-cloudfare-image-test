bed68f59f74a1a481d537ba468053190a5d1b42f
Marc Portier <marc.portier@gmail.com>
2026-02-23 12:10:44 +0100

    adding tracing of colo because cf is using anycast

--- getimgs.sh
+++ getimgs.sh
@@ -7,6 +7,7 @@ base_img_url="https://${base_host}/organizations/1631/2025-10-29-13-50-55/p33000
 # processed images are served here
 proc_host="recranet.app"
 proc_img_url="https://${proc_host}/cdn-cgi/image/format=auto,width=600,height=280,dpr=1,q=85,fit=crop/${base_img_url}"
+proc_trace_url="https://${proc_host}/cdn-cgi/trace"
 
 # output is storede 
 rpt_file="rpt-$(date --iso).txt" 
@@ -19,13 +20,15 @@ echo "----"                         >> ${rpt_file}
 curl "http://ip-api.com/json/"      >> ${rpt_file}
 echo  -e "\n===="                   >> ${rpt_file}
 
-echo "IP of servers (cdn location)" >> ${rpt_file}
-echo "----"                         >> ${rpt_file}
-echo "resolved IP for ${base_host}" >> ${rpt_file}
-host "${base_host}"                 >> ${rpt_file}
-echo "resolved IP for ${proc_host}" >> ${rpt_file}
-host "${proc_host}"                 >> ${rpt_file}
-echo "===="                         >> ${rpt_file}
+# note this will only reveal something if couldflare is using geoDNS
+# but they are not, they use AnyCast in stead
+#echo "IP of servers (cdn location)" >> ${rpt_file}
+#echo "----"                         >> ${rpt_file}
+#echo "resolved IP for ${base_host}" >> ${rpt_file}
+#host "${base_host}"                 >> ${rpt_file}
+#echo "resolved IP for ${proc_host}" >> ${rpt_file}
+#host "${proc_host}"                 >> ${rpt_file}
+#echo "===="                         >> ${rpt_file}
 
 echo "get ${base_img_url}"          >> ${rpt_file}
 echo "----"                         >> ${rpt_file}
@@ -36,6 +39,20 @@ echo "get ${proc_img_url}"          >> ${rpt_file}
 echo "----"                         >> ${rpt_file}
 curl -LI --url "${proc_img_url}"    >> ${rpt_file}
 echo "===="                         >> ${rpt_file}
+
+echo "trace the cdn service"        >> ${rpt_file}
+echo "----"                         >> ${rpt_file}
+curl -L --url "${proc_trace_url}"   >> ${rpt_file}
+echo "===="                         >> ${rpt_file}
+
+
+cat >> ${rpt_file}<< EOM 
+Notes:
+- Check the cf-ray indicator in the headers of the http-responses above they identify the cluster/node that actually was processing the request
+- Additionally check the colo= parameter in the trace response -- it indicates the "cluster" that is dealing with the requests coming from you region
+
+more than 48 hours after initial report to recranet -- the observation remains that requests handled by colo=BRU are faulty most of the time.
+EOM
 
 cat ${rpt_file}
 echo "done"
